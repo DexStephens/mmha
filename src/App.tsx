5d81d4eac4abfe9c2119d6adcea7a203f449d188
tx-smitht <tx.smiththomas@gmail.com>
2023-11-28 13:38:19 -0700

    Remove unnecessary code and logos

--- src/App.tsx
+++ src/App.tsx
@@ -19,31 +19,9 @@ function App() {
         <option value="mission_leader">Mission Leader</option>
       </select>
     </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>You are currently viewing as a   
         {selectedOption === 'missionary' ? ' Missionary' : ' Mission Leader'}
-
-
-
       </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
